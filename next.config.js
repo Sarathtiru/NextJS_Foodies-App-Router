d552974172c817e53d1ec1a63e22cd8ae9db4ed1
Sarath T <mailtsarath@yahoo.com>
2024-02-23 12:29:30 -0700

    test

--- next.config.js
+++ next.config.js
@@ -4,8 +4,22 @@ const nextConfig = {
     //images: { unoptimized: true },
     //reactStrictMode: true,
 
- // Add basePath
-  //basePath: '/NextJS_Foodies-App-Router',
+ /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: '/NextJS_Foodies-App-Router',
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
