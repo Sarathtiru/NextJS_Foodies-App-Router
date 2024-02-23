beea9b6aa93be828f895b10c9b5b8e2a4fa1becf
Sarath T <mailtsarath@yahoo.com>
2024-02-22 23:02:12 -0700

    githubpages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: "export",
+    reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/NextJS_Foodies-App-Router',
 }
 
 module.exports = nextConfig
